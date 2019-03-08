059ecdc9a27f27ae954375062eaac3f5e7ff9a54
Amoo Pedram <Pedroxam@gmail.com>
2019-03-08 07:42:57 +0300

    update !

--- download_m3u8.php
+++ download_m3u8.php
@@ -1,4 +1,14 @@
 <?php
+if(!isset($_GET['dl'])) die('?');
+
+if (substr(php_uname(), 0, 7) == "Windows")
+{
+	define('FFMPEG_PATH', './ffmpeg.exe');  // visit ffmpeg site for download this.
+}
+else
+{
+	define('FFMPEG_PATH', exec('which ffmpeg'));  // or '/usr/bin/ffmpeg'.
+}
 
 $url = trim($_GET['dl']);
 
@@ -6,24 +16,21 @@ download_m3u8($url);
 
 function download_m3u8($url, $dir = '')
 {
-    if (substr(php_uname(), 0, 7) == "Windows")
-    {
-        $ffmpeg  = './ffmpeg.exe'; // download from offical ffmpeg site
-    }
-    else {
-        $ffmpeg = exec('which ffmpeg'); // or '/usr/bin/ffmpeg';
-    }
-    
     $content = file_get_contents($url);
 
-    if (preg_match_all('/(http|https):\/\/.*/', $content, $matches) or preg_match_all('/.+\.ts/', $content, $matches)) {
+    if (preg_match_all('/(http|https):\/\/.*/', $content, $matches) or preg_match_all('/.+\.ts/', $content, $matches))
+	{
         if (!$dir) {
             $dir = "video/" . md5($url);
         }
+		
         makedir($dir);
+		
         echo "dir {$dir}\n\n";
         echo "download ts\n";
+		
         $count = count($matches[0]);
+		
         foreach ($matches[0] as $key => $value) {
             if (strpos($value, 'http') === false) {
                 $parse_url_result = parse_url($url);
@@ -33,15 +40,21 @@ function download_m3u8($url, $dir = '')
                 $url_path_pre = $parse_url_result['scheme'] . "://" . $parse_url_result['host'] . implode('/', $arr) . "/";
                 $value = $url_path_pre . $value;
             }
+			
             $ts_output = "{$dir}/{$key}.ts";
             $cmd = "curl -L -o {$ts_output} '{$value}'";
-            exec($cmd);
+			
+			echo betterExec($cmd); //exec cmd
+			
             echo "\n$cmd\n";
-            if (is_file($ts_output)) {
+			
+            if (is_file($ts_output))
+			{
                 $ts_outputs[] = $ts_output;
-            } else {
-                echo "create ts_output file failed ;\n $cmd";
-                exit();
+            }
+			else {
+				//update
+				makedir($ts_output);
             }
         }
         if ($count > 100) {
@@ -49,48 +62,87 @@ function download_m3u8($url, $dir = '')
         } else {
             $to_concat[] = $ts_outputs;
         }
+		
         echo "concat ts to mp4\n";
+		
         print_r($to_concat);
+		
         foreach ($to_concat as $key => $value) {
+			
             $str_concat = implode('|', $value);
+			
             $mp4_output = "{$dir}/output{$key}.mp4";
-            $cmd = "$ffmpeg -i \"concat:{$str_concat}\" -acodec copy -vcodec copy -absf aac_adtstoasc {$mp4_output}";
-            exec($cmd);
+			
+            $cmd = FFMPEG_PATH . " -i \"concat:{$str_concat}\" -acodec copy -vcodec copy -absf aac_adtstoasc {$mp4_output}";
+			
+			echo betterExec($cmd); //exec cmd
+			
             echo "\n$cmd\n";
-            if (is_file($mp4_output)) {
+			
+            if (is_file($mp4_output))
+			{
                 $mp4_outputs[] = $mp4_output;
-            } else {
+            }
+			else
+			{
                 echo "create mp4_outputs file failed ;\n $cmd";
                 exit();
             }
         }
         $last = "{$dir}/output.mp4";
+		
         if (count($to_concat) > 1) {
-            foreach ($mp4_outputs as $key => $value) {
+			$fileliststr = '';
+            foreach ($mp4_outputs as $key => $value)
+			{
                 $fileliststr .= "file '{$value}'\n";
             }
-            $filelist_file = "filelist.txt";
+			
+			$filelist_file = dirname(__FILE__) . "/filelist.txt";
+			
             file_put_contents($filelist_file, $fileliststr);
-
-            $cmd = "$ffmpeg -f concat -i {$filelist_file} -c copy {$last}";
-            exec($cmd);
-            echo "\n$cmd\n";
-        } else {
+			
+			$cmd = FFMPEG_PATH . " -f concat -i $filelist_file -c copy $path";
+			
+			echo betterExec($cmd); //exec cmd
+			
+        }
+		else
+		{
             $mp4_output = "{$dir}/output{$key}.mp4";
+			
             rename($mp4_output, $last);
         }
 
         if (is_file($last)) {
             $cmd = "rm -rf {$dir}/*ts";
-            exec($cmd);
+			echo betterExec($cmd); //exec cmd
             echo "\n$cmd\n";
 
             echo "\n\nsuccess {$last}\n";
-        } else {
+        }
+		else {
             echo "\n\nfailed\n";
         }
-
     }
+}
+
+function betterExec($cmd)
+{
+	$log = dirname(__FILE__) . '/logs.txt';
+		
+	//windows
+	if (substr(php_uname(), 0, 7) == "Windows")
+	{
+		pclose(popen("start /B " . $cmd . " 1> $log 2>&1", "r"));
+	}
+	//linux
+	else
+	{
+		shell_exec($cmd . " 1> $log 2>&1");
+	}
+	
+	return file_get_contents($log); //return log
 }
 
 
